d3412aa7f5005c346e669ac63338a544c28fc37a
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-02-06 18:33:19 -0500

    Implemented animation state machine with Root Transforms

---
+++ 3dAnimation/Assets/playerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        myAnim.SetFloat("HAxis", h);
+        myAnim.SetFloat("VAxis", v);
+
+    }
+}
